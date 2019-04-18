69fd9612c3ba0c0666c5d9cd32d65fd1484ba63e
Ovidio R. Calvet <calveto@gmail.com>
2019-04-17 21:57:46 -0400

    Initial setup with simple controller

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Server');
+const app = require('express')();
+const userController = require('./controllers/users');
+const port = process.env.APP_PORT || 6000;
+
+app.use('/api/users', userController);
+
+app.listen(port, () => console.log(`Listening on ${port}`));
