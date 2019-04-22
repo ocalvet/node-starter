5557223bf783a44c6c24b8ac8a5065f033efa42e
Ovidio R. Calvet <calveto@gmail.com>
2019-04-22 08:01:02 -0400

    Add real testing examples

--- services/users.spec.js
+++ services/users.spec.js
@@ -1,5 +1,25 @@
-describe('users', function() {
-  it('passing test', function() {
-    expect(true).toBe(true);
+const UsersController = require('./users');
+
+describe('users.findOne', function() {
+  const users = new UsersController();
+  it('throws an exception', function() {
+    expect(() => users.findOne('123')).toThrow();
+  });
+});
+
+describe('users.findAll', () => {
+  const users = new UsersController();
+  it('returns all users', () => {
+    const allUsers = users.findAll();
+    expect(allUsers).toEqual([
+      {
+        id: 1,
+        name: 'test'
+      },
+      {
+        id: 2,
+        name: 'second'
+      }
+    ]);
   });
 });
